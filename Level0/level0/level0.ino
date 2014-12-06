41759899aaa40a34249ff0ab07dd2d51f7fe9d9d
Arun M Kumar <arunkr24@hotmail.com>
2014-12-06 22:21:28 +0530

    Debug message added

--- Level0/level0/level0.ino
+++ Level0/level0/level0.ino
@@ -3,7 +3,7 @@ Sketch for the leve0 of the setup */
 
 #include <Wire.h>
 
-
+#define DEBUG
 #define Load0_R   A0
 #define Load1_R   A1
 #define Load2_R   A3
@@ -100,21 +100,56 @@ void sendWord(unsigned int data){
                       Cyclic Task
 $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$*/
 void cycLoadRead(){
+  #ifdef DEBUG
+  Serial.print("Inside cycLoadRead\n");
+  #endif
+  
   loads[0].DCL = analogRead(loads[0].readPin);
   loads[1].DCL = analogRead(loads[1].readPin);
   loads[2].DCL = analogRead(loads[2].readPin);
  // loads[3].DCL = analogRead(loads[3].readPin);
+ 
+  #ifdef DEBUG
+  Serial.print("Inside cycLoadRead\n");
+  #endif
+}
+
+void cycLoadWrite(){
+  #ifdef DEBUG
+  Serial.print("Inside cycLoadWrite\n");
+  #endif
+  
+  for (int i=0; i<NUM_LOADS; i++}{
+    digitalWrite(loads[i].writePin, loads[i].state);
+  }
+  
+  #ifdef DEBUG
+  Serial.print("Exit cycLoadWrite\n");
+  #endif
 }
 
 void cycLoadCalc(){
+  #ifdef DEBUG
+  Serial.print("Inside cycLoadCalc\n");
+  #endif
+  
   NodeTotalLoad = 0;
   for(int i=0; i<NUM_LOADS; i++){
     NodeTotalLoad += loads[i].DCL;
   }
+  
+  #ifdef DEBUG
+  Serial.print("Exit cycLoadCalc\n");
+  #endif
 }
 
 
 void cycComm(){
+  
+   #ifdef DEBUG
+  Serial.print("Inside cycComm\n");
+  #endif
+  
    /* Cyclical comm handled during cyclically */
    if(HIGH == digitalRead(dataSend)){
      // we need to send data
@@ -135,9 +170,17 @@ void cycComm(){
      Wire.endTransmission();
          Serial.print("Sent the data to Parent\n");
    }
+   
+  #ifdef DEBUG
+  Serial.print("Exit cycComm\n");
+  #endif
 }
 
 void cycPrioCalc(){
+  #ifdef DEBUG
+  Serial.print("Inside cycPrioCalc\n");
+  #endif
+  
   float sum = 0.0, product = 1.0;
   
   for(int i=0; i< NUM_LOADS; i++){
@@ -167,9 +210,17 @@ void cycPrioCalc(){
    //  Serial.print("\n");
   }
    NodePrio = product/sum;
+   
+  #ifdef DEBUG
+  Serial.print("Exit cycPrioCalc\n");
+  #endif
 }
 
 void cycLogic(){
+   #ifdef DEBUG
+  Serial.print("Inside cycLogic\n");
+  #endif
+  
   
   if(NodeTotalLoad < NodeAssignedLoad){
     unsigned char index = 0;
@@ -178,19 +229,43 @@ void cycLogic(){
       if(loads[i].dynPrio > loads[index].dynPrio){
           index = i;      // Lowest Priority-- Highest Value
       }
- 
+   }
+   
+   loads[index].state = LOW;
   }
   
+  else{
+    //Assign whatever each one is demanding
+    for(int i=0; i< NUM_LOADS; i++)
+    {
+      loads[i].state = HIGH;    // alow everyone to function
+    }
+  }
+  
+  #ifdef DEBUG
+  Serial.print("Exit cycLogic\n");
+  #endif
+}  
   
   
 void NodeTask(){
   /*
     Cyclic Task to be executed by the Node
     */
+  #ifdef DEBUG
+  Serial.print("Inside NodeTask\n");
+  #endif
+  
+    cycLogic();
     cycLoadRead();
+    cycLoadWrite();
     cycLoadCalc();
     cycPrioCalc();
     cycComm();
+    
+  #ifdef DEBUG
+  Serial.print("Exit NodeTask\n");
+  #endif
 }
 
 
